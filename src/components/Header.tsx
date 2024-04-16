a91e721f82fa542118817768909e2386ef6054e2
¡Duque <atanaelleonardo@gmail.com>
2024-04-15 21:08:45 -0300

    style: "Update Header component with new layout and styles"
    This commit updates the Header component by wrapping the existing content in a new div with p-5 shadow-md classes for padding and box-shadow. It also adds a new div for

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,21 +9,27 @@ interface HeaderProps {
 export function Header({ children }: HeaderProps) {
   return (
     <>
-      <div className="flex items-center gap-8">
-        <Image
-          src="/logo.png"
-          alt="cesta de compras"
-          width={150}
-          height={100}
-        />
+      <div className="p-5 shadow-md">
+        <div className="flex items-center gap-8">
+          <Image
+            src="/logo.png"
+            alt="cesta de compras"
+            width={150}
+            height={100}
+          />
 
-        <h2 className="flex gap-2 items-center border rounded-full px-10 p-2 bg-slate-200">
-          <LayoutGrid className="w-5 h-5" /> Category
-        </h2>
+          <h2 className="flex gap-2 items-center border rounded-full px-10 p-2 bg-slate-200">
+            <LayoutGrid className="w-5 h-5" /> Category
+          </h2>
 
-        <div className="md:flex gap-3 items-center border rounded-full p-2 m-5 hidden">
-          <Search />
-          <input className="outline-none" type="text" placeholder="Search..." />
+          <div className="md:flex gap-3 items-center border rounded-full p-2 m-5 hidden">
+            <Search />
+            <input
+              className="outline-none"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
         </div>
       </div>
 
